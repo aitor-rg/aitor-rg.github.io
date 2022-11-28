b9ea18a319523d65f4e8a5427784d8098bd64512
Aitor RG <78908328+aitor-rg@users.noreply.github.com>
2022-11-28 19:21:37 +0100

    erase background earthjs

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const canvas = document.querySelector('#bg')
-const renderer = new THREE.WebGLRenderer({canvas });
+const renderer = new THREE.WebGLRenderer({canvas, alpha: true});
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
